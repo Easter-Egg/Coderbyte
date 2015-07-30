2bd9e11a753cc9aaea6f0306e7d684db5a723f48
Taewoo Kim <tu_k@naver.com>
2015-07-30 14:00:37 +0900

    Create WordCount.rb

---
+++ WordCount.rb
@@ -0,0 +1,14 @@
+# have the function WordCount(str) take the str string parameter being passed 
+# and return the number of words the string contains (ie. "Never eat shredded wheat" would return 4). 
+# Words will be separated by single spaces.
+
+def WordCount(str)
+
+  # code goes here
+  return str.split(" ").count
+  
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+WordCount(STDIN.gets)
